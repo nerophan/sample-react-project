8c60f5bdc414a2ff2c5d93fd06bd3fe9d9655588
huynhphan <huynhph@sendo.vn>
2020-05-14 01:19:52 +0700

    Updates

---
+++ precache-manifest.a1a6b34f229d54b1553a87f31f36366c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90adc7f6ffca151d32e4dc56dd060dff",
+    "url": "/sample-react-project/index.html"
+  },
+  {
+    "revision": "10e8cfc8093efc0ceec6",
+    "url": "/sample-react-project/static/css/main.d1357bc8.chunk.css"
+  },
+  {
+    "revision": "245b6e6e7d84f2dc0b8c",
+    "url": "/sample-react-project/static/js/2.0f7441f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample-react-project/static/js/2.0f7441f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e8cfc8093efc0ceec6",
+    "url": "/sample-react-project/static/js/main.abe59502.chunk.js"
+  },
+  {
+    "revision": "df9241acf6404ace7f7b",
+    "url": "/sample-react-project/static/js/runtime-main.80946ed2.js"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/sample-react-project/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "21a8a343e9daf602ac9f938dd4c83a68",
+    "url": "/sample-react-project/static/media/android.21a8a343.svg"
+  },
+  {
+    "revision": "e6985ffa501496180a8b2ac46345bd78",
+    "url": "/sample-react-project/static/media/facebook.e6985ffa.svg"
+  },
+  {
+    "revision": "eaafb39e570bc967a13b20d11a96e3ff",
+    "url": "/sample-react-project/static/media/instagram.eaafb39e.svg"
+  },
+  {
+    "revision": "a345d8c09899861fc3a185a9e8f0dc2f",
+    "url": "/sample-react-project/static/media/ios.a345d8c0.svg"
+  },
+  {
+    "revision": "bfc442cdb702e6a31f95ddc49c3a1636",
+    "url": "/sample-react-project/static/media/linkedin.bfc442cd.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-react-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5f1e64b09b72d1e76cf5aee0aa063d21",
+    "url": "/sample-react-project/static/media/mastercard.5f1e64b0.svg"
+  },
+  {
+    "revision": "b4a60842e974595b1943dff6c994797f",
+    "url": "/sample-react-project/static/media/noun_avatar.b4a60842.svg"
+  },
+  {
+    "revision": "1b56e07f72c1e2dd53fa6e6b3b2455fe",
+    "url": "/sample-react-project/static/media/noun_cart.1b56e07f.svg"
+  },
+  {
+    "revision": "a8fcd56f7fe2ca5ff0536737929e50f8",
+    "url": "/sample-react-project/static/media/paypal.a8fcd56f.svg"
+  },
+  {
+    "revision": "351fdc27bcd0a22bdc84aad3944e06cf",
+    "url": "/sample-react-project/static/media/search.351fdc27.svg"
+  },
+  {
+    "revision": "408dbb94f66da31bab5d2370cf6f68f6",
+    "url": "/sample-react-project/static/media/shopping-bag.408dbb94.svg"
+  },
+  {
+    "revision": "484d48b597679cf99af38fc051ea5943",
+    "url": "/sample-react-project/static/media/subscription-background.484d48b5.png"
+  },
+  {
+    "revision": "a175566b69c5e50d4f1f5e2e11d0291e",
+    "url": "/sample-react-project/static/media/twitter.a175566b.svg"
+  },
+  {
+    "revision": "0069e5c73126826c422c00512eefe9b0",
+    "url": "/sample-react-project/static/media/visa.0069e5c7.svg"
+  },
+  {
+    "revision": "e649788db1090f14b8a27c9a1bc5585e",
+    "url": "/sample-react-project/static/media/youtube.e649788d.svg"
+  }
+]);
